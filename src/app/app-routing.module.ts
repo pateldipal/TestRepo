a4d17da888173b394edb5829da75d22870bf1744
pateldipal <dipalpatel21993@gmail.com>
2020-10-01 11:17:07 +0530

    Matser branch with angular code

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import {IndexComponent} from './index/index.component';
+import {HomeComponent} from './home/home.component';
+import {AboutusComponent} from './aboutus/aboutus.component';
+import {GallaryComponent} from './gallary/gallary.component';
+import {TableComponent} from './table/table.component';
+import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
+
+
+const routes: Routes = [
+  { path: '',   redirectTo: '/home', pathMatch: 'full' },
+  {path: 'home',  component: HomeComponent},
+  {path: 'about', component: AboutusComponent},
+  {path: 'table', component: TableComponent},
+  {path: 'gallary', component: GallaryComponent},
+  { path: '**', component: PageNotFoundComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
